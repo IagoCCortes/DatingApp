dae5227da61b3a36009d40957dcdd150ae7d745c
IagoCCortes <iagocortes@gmail.com>
2019-11-20 15:04:20 -0300

    Client side authentication

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/Auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('Registered successfully');
+    }, error => {
+      console.log('Failed to register');
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('Canceled');
+  }
+
+}
