89bf2277a43ab85c582eeb7c3e3ca4c36bcd4738
IagoCCortes <iagocortes@gmail.com>
2019-11-20 23:22:03 -0300

    Error handling

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -19,7 +19,7 @@ export class RegisterComponent implements OnInit {
     this.authService.register(this.model).subscribe(() => {
       console.log('Registered successfully');
     }, error => {
-      console.log('Failed to register');
+      console.log(error);
     });
   }
 
