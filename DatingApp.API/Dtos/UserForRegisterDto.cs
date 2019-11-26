a954426b62b8987b6d45ac8c8a294ac788abbb8e
IagoCCortes <iagocortes@gmail.com>
2019-11-26 19:34:19 -0300

    Added reactive form to register

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,14 +1,40 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
-    {
+    {       
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
         [Required]
         public string Username { get; set; }
         
         [Required]
         [StringLength(16, MinimumLength = 4, ErrorMessage = "The password must have between 4 and 16 characters.")]
         public string Password { get; set; }
+
+        public DateTime Created { get; set; }  
+
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
